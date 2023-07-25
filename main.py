bccc30eae6d8ae062c979780b3ddddfc52165df2
Alexey <brech_36@mail.ru>
2023-07-25 15:59:47 +0900

    add task 1

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("hi")
+# Задача 1. Дано натуральное число N. Найдите значение выражения: N + NN + NNN
+# N может быть любой длины.
+# N = 132:132 + 132132 + 132132132 = 132264396
+
+def zdacha1(letter=None):
+    N = input(" Введите число: ")
+    expression = [N, N + N, N + N + N]
+    print(expression)
+    summa = [int(letter) for letter in expression]
+    print(sum(summa))
+
+
+zdacha1()
